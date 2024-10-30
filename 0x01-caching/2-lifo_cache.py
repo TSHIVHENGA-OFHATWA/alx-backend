bae4c13f964305adbd53a3cdc806e5011ac93782
ofhatwa <tofhatwa@gmail.com>
2024-10-30 22:52:40 +0200

    adding the updated LIFO caching file

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -16,7 +16,8 @@ class LIFOCache(BaseCaching):
         self.last_key = None
 
     def put(self, key, item):
-        """ Add an item in the cache following LIFO principle """
+        """Add an item in the cache following LIFO principle
+        """
         if key is not None and item is not None:
             self.cache_data[key] = item
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
@@ -26,5 +27,6 @@ class LIFOCache(BaseCaching):
             self.last_key = key
 
     def get(self, key):
-        """ Get an item by key """
-        FOreturn self.cache_data.get(key, None)
+        """Get an item by key
+        """
+        return self.cache_data.get(key, None)
